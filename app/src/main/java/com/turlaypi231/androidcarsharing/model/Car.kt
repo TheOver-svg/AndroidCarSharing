434f6a4b40a296085c624ce2026592887dd4b598
unknown <turlay03102008@gmail.com>
2026-04-05 16:07:12 +0300

    Added data model for enity car

---
+++ app/src/main/java/com/turlaypi231/androidcarsharing/model/Car.kt
@@ -0,0 +1,12 @@
+package com.turlaypi231.androidcarsharing.model
+
+import com.google.android.gms.maps.model.LatLng
+
+
+data class Car(
+    val id: String,
+    val model: String,
+    val location: LatLng,
+    val fuelLevel: Int,
+    val plateNumber: String
+)
