90d0b82817ce98f915f35929568fdb811ee00a70
unknown <turlay03102008@gmail.com>
2026-04-14 12:59:45 +0300

    Added a timer for dynamic update a map

--- app/src/main/java/com/turlaypi231/androidcarsharing/viewModel/MapViewModel.kt
+++ app/src/main/java/com/turlaypi231/androidcarsharing/viewModel/MapViewModel.kt
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.turlaypi231.androidcarsharing.model.Car
 import com.turlaypi231.androidcarsharing.services.RetrofitClient
 import com.turlaypi231.androidcarsharing.view.MapUiState
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -19,8 +20,20 @@ class MapViewModel: ViewModel() {
     init {
         loadCars()
     }
+    init {
+        startAutoRefresh()
+    }
 
-    private fun loadCars() {
+    private fun startAutoRefresh() {
+        viewModelScope.launch {
+            while (true) {
+                loadCars()
+                delay(15000)
+            }
+        }
+    }
+
+    fun loadCars() {
         viewModelScope.launch {
             try {
                 val fetchedCars = RetrofitClient.api.getAllCars()
