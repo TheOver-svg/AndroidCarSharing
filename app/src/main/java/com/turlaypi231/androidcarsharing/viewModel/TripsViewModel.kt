de716b8ce264d5b7dff637c3a8fc8c13cf1f17ce
unknown <turlay03102008@gmail.com>
2026-05-05 20:45:42 +0300

    Created viewmodel for connect to api

--- app/src/main/java/com/turlaypi231/androidcarsharing/viewModel/TripsViewModel.kt
+++ app/src/main/java/com/turlaypi231/androidcarsharing/viewModel/TripsViewModel.kt
@@ -1,4 +1,38 @@
 package com.turlaypi231.androidcarsharing.viewModel
 
-class TripsViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.turlaypi231.androidcarsharing.data.TripResponse
+import com.turlaypi231.androidcarsharing.services.RetrofitClient
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class TripsViewModel : ViewModel() {
+    private val _trips = MutableStateFlow<List<TripResponse>>(emptyList())
+    val trips: StateFlow<List<TripResponse>> = _trips.asStateFlow()
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    fun fetchTrips() {
+        viewModelScope.launch {
+            _isLoading.value = true
+            try {
+                // Переконайся, що викликаєш правильний Api Service
+                val response = RetrofitClient.userApi.getMyTrips()
+                if (response.isSuccessful) {
+                    _trips.value = response.body() ?: emptyList()
+                } else {
+                    Log.e("TripsViewModel", "Помилка: ${response.code()}")
+                }
+            } catch (e: Exception) {
+                Log.e("TripsViewModel", "Помилка мережі: ${e.message}")
+            } finally {
+                _isLoading.value = false
+            }
+        }
+    }
 }
